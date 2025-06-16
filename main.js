a5bf805742fc481fe04160b78a7f75579c9fcf3b
prototipazionevirtuale2002 <prototipazionevirtuale@gmail.com>
2025-06-16 19:57:48 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,277 +1,115 @@
-( () => {
-    "use strict";
-    var e = {
-        5739: (e, t, a) => {
-            a.d(t, {
-                $: () => i
-            });
-            const i = globalThis.gvar ?? {}
-        }
-    }
-      , t = {};
-    function a(i) {
-        var o = t[i];
-        if (void 0 !== o)
-            return o.exports;
-        var r = t[i] = {
-            exports: {}
-        };
-        return e[i](r, r.exports, a),
-        r.exports
-    }
-    a.d = (e, t) => {
-        for (var i in t)
-            a.o(t, i) && !a.o(e, i) && Object.defineProperty(e, i, {
-                enumerable: !0,
-                get: t[i]
-            })
-    }
-    ,
-    a.o = (e, t) => Object.prototype.hasOwnProperty.call(e, t);
-    const i = {
-        dispatchEvent: EventTarget.prototype.dispatchEvent,
-        stopImmediatePropagation: Event.prototype.stopImmediatePropagation,
-        appendChild: Node.prototype.appendChild,
-        map: {
-            clear: Map.prototype.clear,
-            set: Map.prototype.set,
-            has: Map.prototype.has,
-            get: Map.prototype.get
-        },
-        array: {
-            push: Array.prototype.push,
-            includes: Array.prototype.includes
-        },
-        Map,
-        ShadowRoot,
-        HTMLMediaElement,
-        CustomEvent,
-        JSON: {
-            parse: JSON.parse,
-            stringify: JSON.stringify
-        }
-    };
-    let o;
-    a(5739).$;
-    let r, n, c, s, l = [], p = [];
-    function d(e, t, a) {
-        const i = e?.prototype[t];
-        if (!i)
-            return;
-        const o = i.toString();
-        e.prototype[t] = function(...e) {
-            const t = i.apply(this, e);
-            return a(e, this, t),
-            t
-        }
-        ,
-        e.prototype[t].toString = () => o
-    }
-    function u(e, t, a) {
-        t instanceof i.HTMLMediaElement && (i.array.includes.call(l, t) || (i.array.push.call(l, t),
-        r.wiggleOn(t)))
-    }
-    function h(e, t, a) {
-        a instanceof i.ShadowRoot && (i.array.includes.call(p, a) || (i.array.push.call(p, a),
-        r.wiggleOn(a)))
-    }
-    function m(e) {
-        let t = document.getElementById("movie_player");
-        if (t) {
-            try {
-                t.getAvailablePlaybackRates().push(16)
-            } catch (e) {
-                return
-            }
-            s = e => {
-                if (c !== e) {
-                    c = e;
-                    try {
-                        n.activateFor(1e3),
-                        t.setPlaybackRate(e)
-                    } catch (e) {}
-                }
-            }
-            ,
-            r?.send({
-                type: "YT_REQUEST_RATE"
-            }),
-            window.removeEventListener("timeupdate", m, {
-                capture: !0
-            })
-        }
-    }
-    class y {
-        active = !1;
-        dummyAudio = new Audio;
-        ogDesc = {
-            playbackRate: Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, "playbackRate"),
-            defaultPlaybackRate: Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, "defaultPlaybackRate")
-        };
-        coherence = {
-            playbackRate: new Map,
-            defaultPlaybackRate: new Map
-        };
-        constructor() {
-            for (let e of ["playbackRate", "defaultPlaybackRate"]) {
-                const t = this.ogDesc[e];
-                let a = this.coherence[e]
-                  , o = this;
-                try {
-                    Object.defineProperty(HTMLMediaElement.prototype, e, {
-                        configurable: !0,
-                        enumerable: !0,
-                        get: function() {
-                            return o.ogDesc[e].get.call(this),
-                            o.active ? i.map.has.call(a, this) ? i.map.get.call(a, this) : 1 : t.get.call(this)
-                        },
-                        set: function(r) {
-                            !o.active || this instanceof i.HTMLMediaElement || o.ogDesc[e].set.call(this, r);
-                            try {
-                                let e = t.set.call(o.active ? o.dummyAudio : this, r)
-                                  , n = t.get.call(o.active ? o.dummyAudio : this);
-                                return i.map.set.call(a, this, n),
-                                e
-                            } catch (e) {
-                                throw e
-                            }
-                        }
-                    })
-                } catch (e) {}
-            }
-        }
-        activate = () => {
-            this.tempTimeout && (clearTimeout(this.tempTimeout),
-            delete this.tempTimeout),
-            this.active || (this.active = !0,
-            i.map.clear.call(this.coherence.playbackRate),
-            i.map.clear.call(this.coherence.defaultPlaybackRate),
-            l.forEach((e => {
-                i.map.set.call(this.coherence.playbackRate, e, this.ogDesc.playbackRate.get.call(e)),
-                i.map.set.call(this.coherence.defaultPlaybackRate, e, this.ogDesc.defaultPlaybackRate.get.call(e))
-            }
-            )))
-        }
-        ;
-        deactivate = () => {
-            this.tempTimeout && (clearTimeout(this.tempTimeout),
-            delete this.tempTimeout),
-            this.active && (this.active = !1)
-        }
-        ;
-        activateFor = e => {
-            this.active || (this.activate(),
-            this.tempTimeout = setTimeout(this.deactivate, e))
-        }
-    }
-    class g {
-        #e = document.createElement("div");
-        #t = this.#e.attachShadow({
-            mode: "open"
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const container = document.getElementById('canvas-container');
+const playBtn = document.getElementById('playAnimations');
+const loaderElem = document.getElementById('loader');
+const progressBar = document.getElementById('progressBar');
+const loadingText = document.getElementById('loadingText');
+
+let scene, camera, renderer, mixer, model, controls;
+let clock = new THREE.Clock();
+
+init();
+animate();
+
+function init() {
+  // Scene & camera
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 1.5, 3);
+
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x222222);
+  container.appendChild(renderer.domElement);
+
+  // Lights
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  directionalLight.position.set(10, 10, 10);
+  scene.add(directionalLight);
+
+  // Controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 1, 0);
+  controls.update();
+
+  // Loader
+  const loader = new GLTFLoader();
+
+  loader.load(
+    'animazione_web.glb',
+
+    (gltf) => {
+      model = gltf.scene;
+      scene.add(model);
+
+      // Centra e abbassa leggermente il modello
+      model.position.y = -1.2;
+
+      if (gltf.animations && gltf.animations.length > 0) {
+        mixer = new THREE.AnimationMixer(model);
+
+        gltf.animations.forEach((clip) => {
+          const action = mixer.clipAction(clip);
+          action.loop = THREE.LoopOnce;
+          action.clampWhenFinished = true;
+          // Non serve assegnare a clip.userData.action
         });
-        #a = function() {
-            return Math.ceil(1e10 * Math.random()).toString()
-        }();
-        #i = `GS_SERVER_${this.#a}`;
-        #o = `GS_CLIENT_${this.#a}`;
-        constructor() {
-            this.#e.id = "GS_PARASITE",
-            this.#t.addEventListener(this.#o, this.handle, {
-                capture: !0
-            }),
-            document.documentElement.appendChild(this.#e),
-            this.#e.dispatchEvent(new CustomEvent("GS_INIT",{
-                detail: this.#a
-            })),
-            this.#e.remove()
-        }
-        handle = e => {
-            let t;
-            i.stopImmediatePropagation.call(e);
-            try {
-                e.detail && (t = i.JSON.parse(e.detail))
-            } catch (e) {}
-            t && ("SEEK_NETFLIX" === t.type ? function(e) {
-                try {
-                    (function() {
-                        const e = window.netflix.appContext.state.playerApp.getAPI().videoPlayer;
-                        let t = e.getAllPlayerSessionIds().map((t => e.getVideoPlayerBySessionId(t))).filter((e => e.isReady()));
-                        return t.length > 1 ? t.filter((e => e.isPlaying())) : t
-                    }
-                    )().forEach((t => {
-                        let a = 1e3 * e;
-                        try {
-                            let i = t.getElement().querySelector("video").currentTime - t.getCurrentTime() / 1e3;
-                            a = 1e3 * (e - i)
-                        } catch {}
-                        t.seek(a)
-                    }
-                    ))
-                } catch (e) {}
-            }(t.value) : "GHOST" === t.type ? t.off ? n.deactivate() : n.activate() : "YT_RATE_CHANGE" === t.type && t.value && s?.(t.value))
-        }
-        ;
-        send = e => {
-            i.dispatchEvent.call(this.#t, new i.CustomEvent(this.#i,{
-                detail: i.JSON.stringify({
-                    type: "MSG",
-                    data: e
-                })
-            }))
-        }
-        ;
-        wiggleOn = e => {
-            i.appendChild.call(e, this.#e),
-            i.dispatchEvent.call(this.#t, new i.CustomEvent(this.#i,{
-                detail: i.JSON.stringify({
-                    type: "WIGGLE"
-                })
-            })),
-            this.#e.remove()
-        }
+
+        playBtn.style.display = 'block';
+      }
+
+      loaderElem.style.display = 'none';
+      loadingText.style.display = 'none';
+    },
+
+    (xhr) => {
+      if (xhr.lengthComputable) {
+        let percentComplete = (xhr.loaded / xhr.total) * 100;
+        if (percentComplete > 100) percentComplete = 100;
+        progressBar.style.width = percentComplete + '%';
+        loadingText.textContent = `Caricamento modello... ${Math.floor(percentComplete)}%`;
+      }
+    },
+
+    (error) => {
+      console.error('Errore caricamento modello:', error);
+      loadingText.textContent = 'Errore caricamento modello.';
     }
-    !function() {
-        if (o = o ?? navigator.userAgent.includes("Firefox/"),
-        o) {
-            if (window.loadedGsCtx)
-                return;
-            window.loadedGsCtx = !0,
-            function() {
-                if (!location.hostname.includes("soundcloud.com"))
-                    return;
-                const e = AudioContext.prototype.createMediaElementSource;
-                AudioContext.prototype.createMediaElementSource = function(...t) {
-                    return e.apply(this, [document.createElement("audio")])
-                }
-            }()
-        }
-        !function() {
-            if (!location.hostname.includes("pan.baidu.com"))
-                return;
-            let e = navigator.userAgent;
-            e = e.replace("Windows NT", "Windоws NT"),
-            e = e.replace("Macintosh", "Macintоsh"),
-            e = e.replace("Chrome", "Chrоme"),
-            e = e.replace("Firefox", "Firefоx"),
-            e = e.replace("Edg", "Eԁg"),
-            e = e.replace("Safari", "Sаfari");
-            const t = Object.getOwnPropertyDescriptor(Navigator.prototype, "userAgent");
-            Object.defineProperty(Navigator.prototype, "userAgent", {
-                ...t,
-                get: function() {
-                    return e
-                }
-            })
-        }(),
-        n = new y,
-        r = new g,
-        "www.youtube.com" === location.hostname && window.addEventListener("timeupdate", m, {
-            capture: !0
-        }),
-        d(HTMLMediaElement, "play", u),
-        d(HTMLMediaElement, "pause", u),
-        d(HTMLMediaElement, "load", u),
-        d(Element, "attachShadow", h)
-    }()
+  );
+
+  window.addEventListener('resize', onWindowResize);
 }
-)();
+
+function onWindowResize(){
+  camera.aspect = window.innerWidth/window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  const delta = clock.getDelta();
+  if (mixer) mixer.update(delta);
+
+  renderer.render(scene, camera);
+}
+
+playBtn.addEventListener('click', () => {
+  if (!mixer || !model) return;
+
+  mixer.stopAllAction();
+
+  mixer._actions.forEach(action => {
+    action.reset();
+    action.play();
+  });
+});
